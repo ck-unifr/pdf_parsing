7e65010b89356ae488287b9954ffdad36265f789
Kai <chenk.ck03@gmail.com>
2023-09-19 00:09:13 +0800

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -24,11 +24,45 @@ from utils import get_config_variable
 from langchain.prompts import PromptTemplate
 from langchain.document_loaders import PyPDFLoader
 from langchain.chains import LLMChain
+from transformers import AutoTokenizer, AutoModel, AutoConfig
 from langchain.llms import RWKV
 import os
 
 os.environ["RWKV_CUDA_ON"] = '1'
 os.environ["RWKV_JIT_ON"] = '1'
+
+
+class GLM(LLM):
+    max_token: int = 2048
+    temperature: float = 0.8
+    top_p = 0.9
+    tokenizer: object = None
+    model: object = None
+    history_len: int = 1024
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def _llm_type(self) -> str:
+        return "GLM"
+
+    def load_model(self, llm_device="gpu", model_name_or_path=None):
+        model_config = AutoConfig.from_pretrained(
+            model_name_or_path, trust_remote_code=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path, trust_remote_code=True)
+        self.model = AutoModel.from_pretrained(model_name_or_path,
+                                               config=model_config,
+                                               trust_remote_code=True).half().cuda()
+
+    def _call(self, prompt: str, history: List[str] = [], stop: Optional[List[str]] = None):
+        response, _ = self.model.chat(
+            self.tokenizer, prompt,
+            history=history[-self.history_len:] if self.history_len > 0 else [],
+            max_length=self.max_token, temperature=self.temperature,
+            top_p=self.top_p)
+        return response
 
 
 # Sidebar contents
@@ -39,13 +73,24 @@ with st.sidebar:
     This app is an LLM-powered chatbot built using:
     - [Streamlit](https://streamlit.io/)
     - [LangChain](https://python.langchain.com/)
+    - [ChatGLM2-6B](https://github.com/THUDM/ChatGLM2-6B)
     ''')
     add_vertical_space(5)
     st.write('Made by Kai Chen')
 
 
 @st.experimental_singleton
-def load_llm():
+def load_llm_chatglm():
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    config_file = f'{ROOT_DIR[:-3]}config.ini'  # 配置文件的路径
+    model_path = get_config_variable(config_file, 'LLM', 'chatglm2_6b_path')
+    llm = GLM()
+    llm.load_model(model_name_or_path=model_path)
+    return llm
+
+
+@st.experimental_singleton
+def load_llm_rwkv():
     print('load llm')
     strategy = "cuda fp16i8 *20 -> cuda fp16"
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -108,8 +153,8 @@ def main():
             st.write(docs)
 
             # llm = OpenAI()
-            # llm = CustomLLM()
-            llm = load_llm()
+            # llm = load_llm_rwkv()
+            llm = load_llm_chatglm()
             chain = load_qa_chain(llm=llm, chain_type="stuff")
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=query)
